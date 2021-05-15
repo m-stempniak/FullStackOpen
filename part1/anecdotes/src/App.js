61ca46a43a57b138b3e72db6815ae5b87a2c08d3
Mariusz Stempniak <m.stempniak@zoho.com>
2021-05-16 00:54:31 +0200

    part1d final commit

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const anecdotes = [
+        'If it hurts, do it more often',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+    ]
+
+    const [selected, setSelected] = useState(0)
+    const [votes, setVote] = useState(new Array(anecdotes.length).fill(0))
+    const indexOfTheBestAnecdote = votes.indexOf(Math.max(...votes));
+
+    const nextAnecdote = () => setSelected(Math.floor(Math.random() * anecdotes.length));
+    const vote = () => {
+        const newVotes = [...votes];
+        newVotes[selected] += 1;
+        setVote(newVotes)
+    }
+
+    return (
+        <div>
+            <h1>Anecdote of the day</h1>
+            <div>{anecdotes[selected]}</div>
+            <div>has {votes[selected]} votes</div>
+            <button onClick={vote}>vote</button>
+            <button onClick={nextAnecdote}>next anecdote</button>
+            <h1>Anecdote with most votes</h1>
+            <div>{anecdotes[indexOfTheBestAnecdote]}</div>
+        </div>
+    )
 }
 
-export default App;
+export default App
